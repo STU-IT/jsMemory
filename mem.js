387b22129d4fade3a19e729bfaaaa935a4f91d7a
Søren Magnusson <smag@tec.dk>
2018-12-04 14:31:21 +0100

    Sådan nu virker det hele næsten...

--- mem.js
+++ mem.js
@@ -1,16 +1,5 @@
 window.addEventListener("load", function(){
 
-    // cell1 = document.querySelector("table tr:nth-child(1) td:nth-child(1)");
-    // cell2 = document.querySelector("table tr:nth-child(1) td:nth-child(2)");
-    // cell3 = document.querySelector("table tr:nth-child(2) td:nth-child(1)");
-    // cell4 = document.querySelector("table tr:nth-child(2) td:nth-child(2)");
-
-    // cell1.addEventListener("mouseenter", skiftFarve)
-    // cell2.addEventListener("mouseenter", skiftFarve)
-    // cell3.addEventListener("mouseenter", skiftFarve)
-    // cell4.addEventListener("mouseenter", skiftFarve)
-
-
     function skiftFarve() {
         this.style.backgroundColor = "lightblue";
     }
@@ -47,57 +36,136 @@ window.addEventListener("load", function(){
     }
 
     var vendtBrik = null;
+    var antalStik = 0;
+    var gameState = "start"
+    var antalBrikker = 4;
 
-    function klikPaaBrik(){
-        console.log("Brik", this);
-        visNummer(this);
+    function gameOver(message){
+        alert(message); // TODO gør pænere
+        gameState = "start";
+        startGame();
+    }
+
+    function startGame(){
+        buildGamePad(100);
+    }
+
+    const shuffleArray = arr => arr
+        .map(a => [Math.random(), a])
+        .sort((a, b) => a[0] - b[0])
+        .map(a => a[1]);
+
+    function buildGamePad(size){
+        console.log(size);
+        var halfSize = size /2;
+        antalBrikker = size;
+
+        antalStik = 0;
+        stik.innerHTML = antalStik;
+
+        var numbers = Array(size);
+        for (var i = 0; i < halfSize; i++){
+            numbers[i] = Math.ceil(Math.random() * 90 + 10)
+            numbers[halfSize+i] = numbers[i];
+        }
+    
+        numbers = shuffleArray(numbers);
         
-        // hvis der allerede er EN brik som er vendt
-        if (vendtBrik != null) {
-            if (vendtBrik != this) {
-                if (vendtBrik.querySelector("span.number").textContent 
-                    == this.querySelector("span.number").textContent 
-                    ) 
-                {
-                    console.log("MATCH!")
-                    markerStik(this, vendtBrik);
-                    this.removeEventListener("click", klikPaaBrik);
-                    vendtBrik.removeEventListener("click", klikPaaBrik);
-                    vendtBrik = null;
-                }
-                else
-                {
-                    console.log("NO MATCH!");
-                    markerAntiStik(this, vendtBrik);
-                    
-                    var b1 = this;
-                    var b2 = vendtBrik;
+        // find bredde og højde
+        var width = Math.sqrt(size);
+        var height = width;
 
-                    setTimeout( function(){
-                            afMarker(b1, b2);
-                            skjulNummer(b1);
-                            skjulNummer(b2);
-                        },
-                        1000
-                    );
-                    vendtBrik = null;
-                }
+        // ryd tabellen
+        gamePad.innerHTML = "";
+        
+        // lav rækker
+        for (var i = 0; i < height; i++){
+            // en ny række
+            var row = gamePad.insertRow();
+
+            // lav celler
+            for (var j = 0; j < width; j++){
+                // en ny celle
+                var cell = row.insertCell();
+
+                // tilføj spans
+                cell.innerHTML = '<span class="number">'+ numbers[i*width+j] +'</span>';
+                cell.innerHTML += '<span class="blind">:-)</span>';
+                cell.addEventListener("click", klikPaaBrik);
             }
         }
-        else{
-            vendtBrik = this;
+    }
+
+    function klikPaaBrik(){
+        if (gameState != "winningState")
+        {
+            console.log("Brik", this);
+            visNummer(this);
+            
+            // hvis der allerede er EN brik som er vendt
+            if (vendtBrik != null) {
+                if (vendtBrik != this) {
+                    if (vendtBrik.querySelector("span.number").textContent 
+                        == this.querySelector("span.number").textContent 
+                        ) 
+                    {
+                        console.log("MATCH!")
+                        antalStik++;
+                        console.log("Du har nu", antalStik, "stik");
+                        stik.innerHTML = antalStik;
+                        markerStik(this, vendtBrik);
+                        this.removeEventListener("click", klikPaaBrik);
+                        vendtBrik.removeEventListener("click", klikPaaBrik);
+                        vendtBrik = null;
+                        if (antalBrikker / 2 == antalStik)
+                        {
+                            gameState = "winningState";
+                            setTimeout(()=>gameOver("Du har gennemført"), 500);
+                        }
+                    }
+                    else
+                    {
+                        console.log("NO MATCH!");
+                        markerAntiStik(this, vendtBrik);
+                        
+                        var b1 = this;
+                        var b2 = vendtBrik;
+
+                        setTimeout( function(){
+                                afMarker(b1, b2);
+                                skjulNummer(b1);
+                                skjulNummer(b2);
+                            },
+                            1800
+                        );
+                        vendtBrik = null;
+                    }
+                }
+            }
+            else{
+                vendtBrik = this;
+            }
         }
     }
 
-    var tds = document.querySelectorAll("table tr td");
-    for (var i = 0; i < tds.length; i++ ){
-        this.console.log("i:", i, "tds[i]", tds[i]);
-        //tds[i].addEventListener("mouseenter", skiftFarve);
-        //tds[i].addEventListener("mouseleave", fjernFarve);
+    // init
+    // var tds = document.querySelectorAll("table tr td");
+    // for (var i = 0; i < tds.length; i++ ){
+    //     this.console.log("i:", i, "tds[i]", tds[i]);
+    //     //tds[i].addEventListener("mouseenter", skiftFarve);
+    //     //tds[i].addEventListener("mouseleave", fjernFarve);
 
-        tds[i].addEventListener("click", klikPaaBrik);
+    //     tds[i].addEventListener("click", klikPaaBrik);
+    // }
 
+    var knapper = document.querySelectorAll("button[name=gamePadSize]");
+    for (var i = 0; i < knapper.length; i++){
+        knapper[i].addEventListener("click", function(){
+            buildGamePad(this.value);
+        })
     }
+
+    startGame()
 });
 
 function visNummer(element) {
